82462ad08a5d3c98e0894e437a6cc7896b78c5b0
JJJJJJJJJJJJJJJl <costa.joaom@hotmail.com>
2022-12-11 09:18:25 +0100

    day 11

---
+++ day11/puzzle1.cpp
@@ -0,0 +1,130 @@
+#include "../bits/stdc++.h"
+using namespace std;
+
+#define MAX_ROUNDS 20
+
+class Monkey{
+    public:
+        int id;
+        vector<int> items;
+        tuple<string, string> op;
+        tuple<int, tuple<int,int>> test;
+};
+
+int main(){
+    vector<Monkey> monkeys;
+    string line;
+    int k = 0;
+    int cur_monkey_id = 0;
+    Monkey m;
+    while(getline(cin, line)){
+        stringstream stream(line);
+        // monkey id
+        if(k == 0){
+            m.id = cur_monkey_id++;
+        }
+        // monkey items
+        else if(k == 1){
+            string consume;
+            stream >> consume; stream >> consume;
+            while(stream >> consume){
+                if(consume[consume.length()-1] == ','){
+                    consume = consume.substr(0,consume.length()-1);
+                }
+                m.items.push_back(stoi(consume));
+            }
+        }
+        // monkey op
+        else if(k == 2){
+            string consume;
+            stream >> consume; stream >> consume;
+            stream >> consume; stream >> consume;
+            string oprt; stream >> oprt;
+            string value; stream >> value;
+            m.op = make_tuple(oprt, value);
+        }
+        else if(k == 3){
+            // monkey test statement
+            string consume;
+            stream >> consume; stream >> consume;
+            stream >> consume;
+            string sdiv; stream >> sdiv;
+            int div = stoi(sdiv);
+
+            // monkey test true
+            getline(cin, line);
+            stringstream streamtrue(line);
+            streamtrue >> consume; streamtrue >> consume;
+            streamtrue >> consume; streamtrue >> consume;
+            streamtrue >> consume;
+            string strue; streamtrue >> strue;
+            int true_monkey_id = stoi(strue);
+            k++;
+
+            // monkey test false
+            getline(cin, line);
+            stringstream streamfalse(line);
+            streamfalse >> consume; streamfalse >> consume;
+            streamfalse >> consume; streamfalse >> consume;
+            streamfalse >> consume;
+            string sfalse; streamfalse >> sfalse;
+            int false_monkey_id = stoi(sfalse);
+            k++;
+
+            m.test = make_tuple(div, make_tuple(true_monkey_id, false_monkey_id));
+        }
+        k++;
+        if(k == 7){
+            monkeys.push_back(m);
+            m.items.clear();
+            k = 0;
+        }
+    }
+    monkeys.push_back(m);
+    m.items.clear();
+
+    int round = 1;
+    int n = monkeys.size();
+    int inspections[n];
+    memset(inspections, 0, sizeof(inspections));
+    while(round < MAX_ROUNDS+1){
+        for(int i=0; i<n; i++){
+            int items_n = monkeys[i].items.size();
+            if(items_n == 0) continue;
+            inspections[i] += items_n;
+            int worry_level;
+            for(int j=0; j<items_n; j++){
+                int item = monkeys[i].items[j];
+                worry_level = item;
+                if(get<0>(monkeys[i].op) == "*"){
+                    if(get<1>(monkeys[i].op) == "old"){
+                        worry_level *= worry_level;
+                    }
+                    else{
+                        worry_level *= stoi(get<1>(monkeys[i].op));
+                    }
+                }
+                else{
+                    if(get<1>(monkeys[i].op) == "old"){
+                        worry_level += worry_level;
+                    }
+                    else{
+                        worry_level += stoi(get<1>(monkeys[i].op));
+                    }
+                }
+                worry_level = floor(worry_level / 3);
+                if(worry_level % get<0>(monkeys[i].test) == 0){
+                    monkeys[get<0>(get<1>(monkeys[i].test))].items.push_back(worry_level);
+                }
+                else{
+                    monkeys[get<1>(get<1>(monkeys[i].test))].items.push_back(worry_level);
+                }
+            }
+            monkeys[i].items.clear();
+        }
+        round++;
+    }
+    sort(inspections, inspections+n);
+    cout << inspections[n-1]*inspections[n-2] << endl;
+    return 0;
+}
